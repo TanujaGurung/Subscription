ac53a9340eb46f03c615dcb6ecf049c2584dc514
tanuja1 <tanuja@resyntech.com>
2022-05-25 18:16:50 +0530

    added env file and gitignore file

--- app/src/components/StripeContainer.js
+++ app/src/components/StripeContainer.js
@@ -3,10 +3,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import PaymentOption from "./PaymentOption";
 
-const PUBLIC_KEY =
-  "pk_test_51Kc8iQB6GMJd3WhhN9w1VNKhKol74KfnP3GiXpQ7YRHkL5onDGcPNJur2fao2p6oTi1uHT3KZevBHMtnhtEge8hR00QEOI9TxP";
-
-const stripeTestPromise = loadStripe(PUBLIC_KEY);
+const stripeTestPromise = loadStripe(process.env.REACT_APP_API_KEY);
 
 export default function StripeContainer(props) {
   return (
